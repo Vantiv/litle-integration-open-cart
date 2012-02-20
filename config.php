c44983020f17c1804c0d5718329b2594edfcd334
Gregory,Dake <gdake@litle.com>
2012-02-20 16:02:57 -0500

    Adding config to ignore list

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+define('HTTP_IMAGE', 'http://localhost/opencart/image/');
+define('HTTP_ADMIN', 'http://localhost/opencart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+define('HTTPS_IMAGE', 'http://localhost/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/local/litle-home/gdake/opencart/catalog/');
+define('DIR_SYSTEM', '/usr/local/litle-home/gdake/opencart/system/');
+define('DIR_DATABASE', '/usr/local/litle-home/gdake/opencart/system/database/');
+define('DIR_LANGUAGE', '/usr/local/litle-home/gdake/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/usr/local/litle-home/gdake/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/usr/local/litle-home/gdake/opencart/system/config/');
+define('DIR_IMAGE', '/usr/local/litle-home/gdake/opencart/image/');
+define('DIR_CACHE', '/usr/local/litle-home/gdake/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/usr/local/litle-home/gdake/opencart/download/');
+define('DIR_LOGS', '/usr/local/litle-home/gdake/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
