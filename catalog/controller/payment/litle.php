60118ccb776694190b3c27958b0b13dd4bdcf712
arch85 <archit.agarwal@gmail.com>
2012-03-06 13:13:56 -0500

    wip

--- catalog/controller/payment/litle.php
+++ catalog/controller/payment/litle.php
@@ -1,4 +1,6 @@
 <?php
+require_once($vqmod->modCheck(DIR_SYSTEM . 'library/litle/LitleOnline.php'));
+
 class ControllerPaymentLitle extends Controller {
 	protected function index() {
 		$this->language->load('payment/litle');
@@ -6,7 +8,7 @@ class ControllerPaymentLitle extends Controller {
 		$this->data['text_credit_card'] = $this->language->get('text_credit_card');
 		$this->data['text_wait'] = $this->language->get('text_wait');
 		
-		$this->data['entry_cc_owner'] = $this->language->get('entry_cc_owner');
+		$this->data['entry_cc_type'] = $this->language->get('entry_cc_type');
 		$this->data['entry_cc_number'] = $this->language->get('entry_cc_number');
 		$this->data['entry_cc_expire_date'] = $this->language->get('entry_cc_expire_date');
 		$this->data['entry_cc_cvv2'] = $this->language->get('entry_cc_cvv2');
@@ -21,6 +23,12 @@ class ControllerPaymentLitle extends Controller {
 				'value' => sprintf('%02d', $i)
 			);
 		}
+		
+		$this->data['cardTypes'] = array();
+		$this->data['cardTypes'][] = array( 'text' => "Visa", 'value' => "VI" );
+		$this->data['cardTypes'][] = array( 'text' => "Mastercard", 'value' => "MC" );
+		$this->data['cardTypes'][] = array( 'text' => "American Express", 'value' => "AX" );
+		$this->data['cardTypes'][] = array( 'text' => "Discover", 'value' => "DI" );
 		
 		$today = getdate();
 
@@ -42,18 +50,47 @@ class ControllerPaymentLitle extends Controller {
 		$this->render();		
 	}
 	
+	public function getAddressInfo($addressType)
+	{
+		$this->load->model('checkout/order');
+		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+
+		$retArray = array();
+ 		$retArray["firstName"] = XMLFields::returnArrayValue($order_info, ($addressType + "_firstname") );
+ 		//$retArray["middleInitial"]= XMLFields::returnArrayValue($hash_in, ($addressType + "_lastname") );
+ 		$retArray["lastName"] = XMLFields::returnArrayValue($order_info, ($addressType + "_lastname") );
+		//$retArray["name"] = XMLFields::returnArrayValue($order_info, ($addressType + "_firstname") );
+ 		$retArray["companyName"] = XMLFields::returnArrayValue($order_info, ($addressType + "_company") );
+ 		$retArray["addressLine1"] = XMLFields::returnArrayValue($order_info, ($addressType + "_address_1") );
+ 		$retArray["addressLine2"] = XMLFields::returnArrayValue($order_info, ($addressType + "_address_2") );
+ 		$retArray["city"] = XMLFields::returnArrayValue($order_info, ($addressType + "_city") );
+ 		$retArray["state"] = XMLFields::returnArrayValue($order_info, ($addressType + "_firstname") );
+ 		$retArray["zip"] = XMLFields::returnArrayValue($order_info, ($addressType + "_postcode") );
+ 		$retArray["country"] = XMLFields::returnArrayValue($order_info, ($addressType + "_country") );
+ 		$retArray["email"] = XMLFields::returnArrayValue($order_info, "email" );
+ 		$retArray["phone"] = XMLFields::returnArrayValue($order_info, "telephone" );
+ 		
+ 		return $retArray;
+	}
+	
+	public function getCreditCardInfo()
+	{
+		$this->load->model('checkout/order');
+		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+		
+		$retArray = array();
+		$retArray["type"] = $this->request->post['cc_type'];
+ 		$retArray["number"] = str_replace(' ', '', $this->request->post['cc_number']);
+ 		//TODO: fix the logic for expDate
+ 		$retArray["expDate"] = $this->request->post['cc_expire_date_month'] . ($this->request->post['cc_expire_date_year']-2000);
+ 		$retArray["cardValidationNum"] = $this->request->post['cc_cvv2'];
+ 		
+ 		return $retArray;
+	}
+	
 	public function send() {
-// 		if ($this->config->get('authorizenet_aim_server') == 'live') {
-//     		$url = 'https://secure.authorize.net/gateway/transact.dll';
-// 		} elseif ($this->config->get('authorizenet_aim_server') == 'test') {
-// 			$url = 'https://test.authorize.net/gateway/transact.dll';		
-// 		}	
-		
-// 		//$url = 'https://secure.networkmerchants.com/gateway/transact.dll';	
-		
  		$this->load->model('checkout/order');
-		
-// 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+ 		$order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 		
 //         $data = array();
 
@@ -88,51 +125,60 @@ class ControllerPaymentLitle extends Controller {
 // 		if ($this->config->get('authorizenet_aim_mode') == 'test') {
 // 			$data['x_test_request'] = 'true';
 // 		}	
-				
-		//$curl = curl_init($url);
-
-		//curl_setopt($curl, CURLOPT_PORT, 443);
-		//curl_setopt($curl, CURLOPT_HEADER, 0);
-		//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-		//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		//curl_setopt($curl, CURLOPT_FORBID_REUSE, 1);
-		//curl_setopt($curl, CURLOPT_FRESH_CONNECT, 1);
-		//curl_setopt($curl, CURLOPT_POST, 1);
-		//curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
-		//curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-		//curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
- 
-		//$response = curl_exec($curl);
 		
 		$json = array();
-		
-// 		if (curl_error($curl)) {
-// 			$json['error'] = 'CURL ERROR: ' . curl_errno($curl) . '::' . curl_error($curl);
-			
-// 			$this->log->write('AUTHNET AIM CURL ERROR: ' . curl_errno($curl) . '::' . curl_error($curl));	
-// 		} elseif ($response) {
-// 			$i = 1;
-			
-		
-		$doingAuth = $this->config->get('litle_transaction') == 'auth';
+ 		$hash_in = array(
+ 					'orderId'=> $order_info['order_id'],
+ 					'amount'=> 5000,//$order_info['total'],
+ 					'orderSource'=> "ecommerce",
+ 					//'customerInfo'=> $order_info['payment_firstname'],
+ 					'billToAddress'=> $this->getAddressInfo("payment"),
+ 					'shipToAddress'=> $this->getAddressInfo("shipping"),
+ 					'card'=> $this->getCreditCardInfo()//,
+ 					//'paypal'=> $order_info['payment_firstname'],
+ 					//'token'=> $order_info['payment_firstname'],
+ 					//'paypage'=> $order_info['payment_firstname'],
+ 					//'billMeLaterRequest'=> $order_info['payment_firstname'],
+ 					//'cardholderAuthentication'=> $order_info['payment_firstname'],
+ 					//'processingInstructions'=> $order_info['payment_firstname'],
+ 					//'pos'=>(XMLFields::pos($hash_in['pos'])),
+ 					//'customBilling'=>(XMLFields::customBilling($hash_in['customBilling'])),
+ 					//'taxBilling'=>(XMLFields::taxBilling($hash_in['taxBilling'])),
+ 					//'enhancedData'=>(XMLFields::enhancedData($hash_in['enhancedData'])),
+ 					//'amexAggregatorData'=>(XMLFields::amexAggregatorData($hash_in['amexAggregatorData'])),
+ 					//'allowPartialAuth'=>$hash_in['allowPartialAuth'],
+ 					//'healthcareIIAS'=>(XMLFields::healthcareIIAS($hash_in['healthcareIIAS'])),
+ 					//'filtering'=>(XMLFields::filteringType($hash_in['filtering'])),
+ 					//'merchantData'=>(XMLFields::filteringType($hash_in['merchantData'])),
+ 					//'recyclingRequest'=>(XMLFields::recyclingRequestType($hash_in['recyclingRequest']))
+ 		);
+ 		
+ 		$litleResponseMessagePrefix = "";
+ 		$litleRequest = new LitleOnlineRequest();
+		$doingAuth = $this->config->get('litle_transaction') == "auth";
 		if($doingAuth) {
 			//auth txn
-			$response = array('code'=>'000', 'message'=>'Approved', 'litleTxnId'=>'123456789012345678');
+			$response = $litleRequest->authorizationRequest($hash_in);
+			$litleResponseMessagePrefix = "LitleCapturableTxn: ";
 		}
 		else {
 			//sale txn
-			$response = array('code'=>'000', 'message'=>'Approved', 'litleTxnId'=>'876543210987654321');
+			$response = $litleRequest->saleRequest($hash_in);
+			$litleResponseMessagePrefix = "LitleRefundableTxn: ";
 		}
 		
-		$code = $response['code'];
-		$message = $response['message'] . " \n Transaction ID: " . $response['litleTxnId'] . " \n";
+		$code = XMLParser::getNode($response, "response");
+		$litleValidationMessage = XMLParser::getNode($response, "message");
+		$litleTxnId = XMLParser::getNode($response, "litleTxnId");
 		
-		if($code == '000') { //Success
+		$message = $litleResponseMessagePrefix . $litleValidationMessage . " \n Transaction ID: " . $litleTxnId . " \n";
+		
+		if($code == "000") { //Success
 			if($doingAuth) {
 				$orderStatusId = 1; //Pending
 			}
 			else {
-				$orderStatusId = 2; //Processing
+				$orderStatusId = 5; //Processing
 			}
 		}
 		else {
@@ -140,7 +186,7 @@ class ControllerPaymentLitle extends Controller {
 		}
 		
 		$this->model_checkout_order->confirm(
-			$this->session->data['order_id'], 
+			$order_info['order_id'], 
 			$orderStatusId,
 			$message,
 			true
