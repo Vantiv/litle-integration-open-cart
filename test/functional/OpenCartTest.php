c72e9e7050352145a300e354c67ff6556b8c272e
pshah91 <pshah91@ccs.neu.edu>
2014-03-19 13:57:27 -0400

    fixing version issue

--- test/functional/OpenCartTest.php
+++ test/functional/OpenCartTest.php
@@ -25,7 +25,7 @@ class OpenCartTest extends PHPUnit_Framework_TestCase {
 		if (file_exists ( $path )) {
 			require_once ($path);
 		} else {
-			throw new Exception ( 'OpenCart has to be installed first!' .  $_ENV['OPENCART_VERSION'] . '.' );
+			throw new Exception ( 'OpenCart has to be installed first!' .  $_OPENCART . '.' );
 		}
 	}
 	public function __construct() {
